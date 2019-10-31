f132272f99ef44ac1fd26e61d34922df2cab87df
karel-rehor <karel.k.rehor@email.cz>
2019-10-31 13:36:42 +0100

    smoke test run

--- connect-check.sh
+++
@@ -1,6 +0,0 @@
-!#/usr/bin/env bash
-
-sleep 30
-
-sudo netstat -tlnp
-curl -v --connect-timeout 60 --max-time 60 http://localhost:9999/debug/flush
---
+++ test-run.sh
@@ -0,0 +1,15 @@
+!#/usr/bin/env bash
+
+sleep 30
+
+sudo netstat -tlnp
+curl -v --connect-timeout 60 --max-time 60 http://localhost:9999/debug/flush
+
+git clone https://github.com/bonitoo-io/selenium-accept-infl2.git
+
+cd selenium-accept-infl2/
+
+npm install 
+
+npm test -- features/onboarding/onboarding.feature
+
