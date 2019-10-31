3dc1a4e432876cfdf3a6f4be255a4984bcd7329b
karel-rehor <karel.k.rehor@email.cz>
2019-10-31 13:19:47 +0100

    try parallel images

--- connect-check.sh
+++ connect-check.sh
@@ -1,4 +1,6 @@
 !#/usr/bin/env bash
 
+sleep 30
+
 sudo netstat -tlnp
-curl -v --connect-timeout 60 --max-time 60 http://localhost:9999/debug/flush
+curl -v --connect-timeout 60 --max-time 60 http://localhost:9999
